38f2bb74b3fe14527ae9cb6cfad1fb848313a992
Bogdan Bogotov <63542551+tokyodaito@users.noreply.github.com>
2025-06-10 00:43:42 +0300

    Add risk corridor with macro data

--- service/src/main/kotlin/service/di/ServiceModule.kt
+++ service/src/main/kotlin/service/di/ServiceModule.kt
@@ -1,10 +1,16 @@
 package service.di
 
 import data.market.MoexDataSource
+import data.macro.MacroDataSource
 import org.koin.dsl.module
 import service.DcaService
 
 val serviceModule =
     module {
-        single { DcaService { get<MoexDataSource>().fetchMarketData() } }
+        single {
+            DcaService(
+                fetchMarketData = { get<MoexDataSource>().fetchMarketData() },
+                fetchMacroData = { date -> get<MacroDataSource>().fetchMacroData(date) },
+            )
+        }
     }
