2b615b6412901ccf956c24d4f0604fdbd16ab0e6
Bogdan Bogotov <63542551+tokyodaito@users.noreply.github.com>
2025-06-10 01:38:02 +0300

    Fix BotCommandHandler tests

--- telegram/src/test/kotlin/BotCommandHandlerTest.kt
+++ telegram/src/test/kotlin/BotCommandHandlerTest.kt
@@ -16,7 +16,8 @@ class BotCommandHandlerTest {
     fun `set monthly flow updates config`() =
         runBlocking {
             val repo = InMemoryChatConfigRepository()
-            val ds = DcaService { MarketData(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.1) }
+            val md = MarketData(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.1)
+            val ds = DcaService({ md }) { MacroData(70.0, 10.0, 9.5) }
             val handler = BotCommandHandler(repo, ds, "http://localhost")
             val portfolio = Portfolio(0.0, 0.0, 0.0)
             handler.handle(1, "/set_monthly_flow 150000", portfolio)
@@ -38,7 +39,7 @@ class BotCommandHandlerTest {
     @Test
     fun `open webapp returns webapp response`() = runBlocking {
         val repo = InMemoryChatConfigRepository()
-        val md = MarketData(2650.0, 3000.0, 2700.0, 28.0, 6.3, 13.0, 10.5)
+        val md = MarketData(2650.0, 3000.0, 2700.0, 28.0, 6.3, 13.0, 10.5, 0.2)
         val ds = DcaService({ md }) { MacroData(70.0, 10.0, 9.5) }
         val handler = BotCommandHandler(repo, ds, "http://localhost")
         val portfolio = Portfolio(0.0, 0.0, 0.0)
